6f480f323e027c9c1a1c36a0ab44a0b44acbf021
Matheus Bastos <epherex@gmail.com>
2018-09-03 19:50:06 -0300

    Corrigir headers para incluir mixer

--- headers.h
+++ headers.h
@@ -17,10 +17,12 @@
     #include <SDL.h>
     #include <SDL_image.h>
     #include <SDL_ttf.h>
+    #include <SDL_mixer.h>
 #else
     #include <SDL2/SDL.h>
     #include <SDL2/SDL_image.h>
     #include <SDL2/SDL_ttf.h>
+    #include <SDL2/SDL_mixer.h>
 #endif
 
 #include <stdio.h>
